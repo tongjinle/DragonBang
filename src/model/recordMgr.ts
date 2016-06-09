7983354856c6f16fff341c3269f64ca0dda0244d
tongjinle <1328192478@qq.com>
2016-06-09 13:15:18 +0800

    完成recordMgr的测试

--- src/model/recordMgr.ts
+++ src/model/recordMgr.ts
@@ -1,11 +1,108 @@
 class RecordMgr {
 	static mgr;
+
 	constructor() {
-		
+
 	}
 
-	getInstance (){
+	static getInstance(): RecordMgr {
 		var mgr = RecordMgr.mgr = RecordMgr.mgr || new RecordMgr();
 		return mgr;
 	}
+
+	init(isForce: boolean = false) {
+		if (isForce || egret.localStorage.getItem('hasInit') !== '1') {
+			egret.localStorage.setItem('dragonList', '[]');
+			egret.localStorage.setItem('bulletPowerLevel', '1');
+			egret.localStorage.setItem('bulletSpeedLevel', '1');
+			egret.localStorage.setItem('armorLevel', '1');
+			egret.localStorage.setItem('coin', '0');
+			egret.localStorage.setItem('gateList', '[]');
+			egret.localStorage.setItem('resList', '{}');
+			// 标志
+			egret.localStorage.setItem('hasInit', '1');
+		}
+	}
+
+	// 读取龙仔列表
+	getDragonList(): string[] {
+		return JSON.parse(egret.localStorage.getItem('dragonList'));
+	}
+
+	// 新增龙仔
+	addDragon(dragonName: string): void {
+		var dragonList = this.getDragonList();
+		dragonList.push(dragonName);
+		egret.localStorage.setItem('dragonList', JSON.stringify(dragonList));
+	}
+
+	// 读取/设置母舰子弹威力等级
+	getBulletPowerLevel(): number {
+		return parseInt(egret.localStorage.getItem('bulletPowerLevel'));
+	}
+
+	setBulletPowerLevel(powerLevel: number): void {
+		egret.localStorage.setItem('bulletPowerLevel', powerLevel.toString());
+	}
+
+	// 读取/设置母舰子弹速度等级
+	getBulletSpeedLevel(): number {
+		return parseInt(egret.localStorage.getItem('bulletSpeedLevel'));
+	}
+
+	setBulletSpeedLevel(speedLevel: number): void {
+		egret.localStorage.setItem('bulletSpeedLevel', speedLevel.toString());
+	}
+
+
+	// 读取/设置母舰装甲等级
+	getArmorLevel(): number {
+		return parseInt(egret.localStorage.getItem('armorLevel'));
+	}
+
+	setArmoLevel(armorLevel: number): void {
+		egret.localStorage.setItem('armorLevel', armorLevel.toString());
+	}
+
+
+	// 读取/设置金币
+	getCoin(): number {
+		return parseInt(egret.localStorage.getItem('coin'));
+	}
+
+	setCoin(coin: number): void {
+		egret.localStorage.setItem('coin', coin.toString());
+	}
+
+	// 读取通关列表信息
+	getGateList(): number[] {
+		return JSON.parse(egret.localStorage.getItem('gateList'));
+	}
+
+	// 设置某关信息
+	setGate(index: number, score: number): void {
+		var gateList = this.getGateList();
+		gateList[index] = score;
+		egret.localStorage.setItem('gateList', JSON.stringify(gateList));
+	}
+
+	// 读取物品列表
+	getResList(): { [res: string]: number }[] {
+		return JSON.parse(egret.localStorage.getItem('resList'));
+	}
+
+	// 新增物品
+	addRes(res: string, count: number = 1): void {
+		var resList = this.getResList();
+		resList[res] = resList[res] || 0 ;
+		resList[res] += count;
+		egret.localStorage.setItem('resList', JSON.stringify(resList));
+	}
+
+	// 消耗物品
+	removeRes(res: string, count: number = 1): void {
+		this.addRes(res, -count);
+	}
+
+
 }
--- src/test/recordSpec.ts
+++ src/test/recordSpec.ts
@@ -1,35 +1,75 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 
 
-describe('游戏信息记录器',()=>{
-   it('清空信息',()=>{
-       
-   }); 
-   
-   
-   it('龙仔list',()=>{
+describe('游戏信息记录器', () => {
+    var mgr = RecordMgr.getInstance();
 
-   });
-   
-   it('母舰子弹威力',()=>{
+    afterAll(() => {
+        mgr.init(true);
+    });
 
-   });
-   
-   it('母舰子弹速度',()=>{
 
-   });
-   
-   it('母舰装甲',()=>{
+    it('初始化', () => {
+        mgr.init();
+        expect(mgr.getCoin()).toBe(0);
+        mgr.setCoin(100);
+        expect(mgr.getCoin()).toBe(100);
+        mgr.init();
+        expect(mgr.getCoin()).toBe(100);
+        // 强制刷新会清空数据
+        mgr.init(true);
+        expect(mgr.getCoin()).toBe(0);
+    });
 
-   });
-   
-   it('金币',()=>{
+    it('龙仔list', () => {
+        // 读取list
+        expect(mgr.getDragonList()).toEqual([]);
+        // 新增龙仔
+        mgr.addDragon('dragon1');
+        mgr.addDragon('dragon2');
+        expect(mgr.getDragonList()).toEqual(['dragon1','dragon2']);
+    });
 
-   });
-   
-   it('通关信息',()=>{
+    it('母舰子弹威力', () => {
+        expect(mgr.getBulletPowerLevel()).toBe(1);
+        mgr.setBulletPowerLevel(2);
+        expect(mgr.getBulletPowerLevel()).toBe(2);
+    });
 
-   });
+    it('母舰子弹速度', () => {
+        expect(mgr.getBulletSpeedLevel()).toBe(1);
+        mgr.setBulletSpeedLevel(2);
+        expect(mgr.getBulletSpeedLevel()).toBe(2);
+    });
+
+    it('母舰装甲', () => {
+        expect(mgr.getArmorLevel()).toBe(1);
+        mgr.setArmoLevel(2);
+        expect(mgr.getArmorLevel()).toBe(2);
+    });
+
+    it('金币', () => {
+        expect(mgr.getCoin()).toBe(0);
+        mgr.setCoin(100);
+        expect(mgr.getCoin()).toBe(100);
+    });
+
+    it('通关信息', () => {
+        expect(mgr.getGateList()).toEqual   ([]);
+        mgr.setGate(5,100);
+        mgr.setGate(3,200);
+        expect(mgr.getGateList()).toEqual([null,null,null,200,null,100]);
+        
+    });
+
+    it('物品栏', () => {
+        expect(mgr.getResList()).toEqual({});
+        mgr.addRes('box',2);
+        mgr.addRes('pencil',100);
+        mgr.addRes('box',5);
+        mgr.removeRes('pencil');
+        expect(mgr.getResList()).toEqual({'box':7,'pencil':99});
+    });
 
 
 
